39353f76ad7a2fb47e7d2beb65fdd751c97e51bd
codendive <codendive@gmail.com>
2025-02-23 19:09:33 +0100

    Add theme toggle functionality and dropdown menu component; update header layout

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,19 +1,22 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { APP_NAME } from '@/lib/constants'
+import ModeToggle from './mode-toggle'
 
 const Header = () => {
   return (
-    <header className='border-b'>
-      <Link className='flex items-center' href='/'>
+    <header className='border-b px-3 py-2 flex justify-between items-center'>
+      <Link href='/'>
         <Image
+          className='rounded-full'
           src='/images/logo.png'
           alt={`${APP_NAME} logo`}
-          height={80}
-          width={80}
+          height={70}
+          width={70}
           priority
         />
       </Link>
+      <ModeToggle />
     </header>
   )
 }
